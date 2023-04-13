39848c958d8a2e411c937a436c31b6d53980901e
Tiffany Li <tiffany.li4@student.tdsb.on.ca>
2023-04-13 00:28:24 -0400

    Added movie genres and changed Jaws to Turning Red.

--- script.js
+++ script.js
@@ -4,36 +4,51 @@ const getTMDBData = async (url) => {
   return (await axios.get(url)).data;
 };
 
-const createMovieTile = (id, poster, title, date, vote_average, runtime, description) => {
+const createMovieTile = (id, poster, title, date, rating, genres, runtime, collection, description) => {
   const tile = document.createElement("div");
 
   const details = document.createElement("div");
 
   const img = document.createElement("img");
   const movieTitle = document.createElement("h1");
-  // const movieGenres = document.createElement("div");
-  // movieGenres.innerHTML = genres[0];
   const dateReleased = document.createElement("h2");
-  const movieRating = document.createElement("h2"); 
+  const movieRating = document.createElement("h3"); 
+  const movieGenres = document.createElement("div");///////////////
   const duration = document.createElement("h3");
+  const movieCollection = document.createElement("h3");
   const movieDescription = document.createElement("h4");
   const trailerButton = document.createElement("button");
 
   tile.classList.add("tile");
   img.src = `https://image.tmdb.org/t/p/w500/${poster}`;
   movieTitle.innerText = title;
-  // movieGenres.innerText = genres;
+  
+    let genresArray = [];
+    for (let i = 0; i < genres.length; i++) {
+      genresArray[i] = ` ${genres.at(i).name}`;
+      console.log(genresArray);
+    }
+  
+    movieGenres.innerText = "Genre(s): " + genresArray;
+
   dateReleased.innerText = date;
-  movieRating.innerText = vote_average;
+  movieRating.innerText = "Rating: " + rating + "/10";
   duration.innerText = "Movie duration: " + runtime + " minutes";
+  
+  !collection
+    ?movieCollection.innerText = "This movie is currently not part of a collection."
+    :movieCollection.innerText = collection;
+  
+  
   movieDescription.innerText = description;
   trailerButton.innerText = "Trailer";
 
   details.append(movieTitle);
-  // details.append(movieGenres);
+  details.append(movieGenres);/////////////////////////
   details.append(dateReleased);
   details.append(movieRating);
   details.append(duration);
+  details.append(movieCollection);
   details.append(movieDescription);
 
   console.log(tile);
@@ -53,6 +68,8 @@ const createMovieTile = (id, poster, title, date, vote_average, runtime, descrip
     !trailer.length
       ? alert("There are no trailors for this film.")
       : window.open(`https://www.youtube.com/watch?v=${trailer.at(0).key}`);
+      console.log(trailer.at(0).key);
+      console.log(trailer);
   });
 
   return tile;
@@ -67,19 +84,39 @@ getMovieButton.addEventListener("click", async () => {
   );
   console.log(movieData);
 
+  // let genresArray = [];
+  // for (let i = 0; i < movieData.genres.length; i++) {
+  //   genresArray = movieData.genres.at(i).name;
+  //   //console.log[movieData.genres.at(i).name];
+  //   console.log(genresArray);
+  // }
   const tile = createMovieTile(
     movieData.id,
     movieData.poster_path,
     movieData.title,
     movieData.release_date,
+    movieData.vote_average,
+    movieData.genres,///////////////
     movieData.runtime,
+    movieData.genresArray,
     movieData.overview
   );
+    //console.log(movieData.belongs_to_collection);////returns object object
+    
+    // movieData.genres.filter((genres) => {
+    //   // return genres.type === "genre";
+    // })
+
+
+    // for (let i = 0; i < movieData.genres.length; i++) {
+    //   // console.log(movieData.genres.at(i).name);
+    //   const genresArray = [movieData.genres.at(i).name];
+    // }
+    
 
   movieDisplay.replaceChildren(tile);
 });
 
 //jaws: 578 spiderm: 429617 ironman2: 10138 avatar: 76600 Shrek: 808 puss:315162 super mario: 502356 httyd: 166428 up: 14160 star wars: 11
 
-//filter for genres ?
 //country of origin?
