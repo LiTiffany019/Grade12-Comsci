a7987e45909708485dc7690fe90bb129ef6d1c07
Tiffany Li <tiffany.li4@student.tdsb.on.ca>
2023-05-22 01:21:21 -0400

    .

--- src/views/PurchaseView.vue
+++ src/views/PurchaseView.vue
@@ -1,6 +1,12 @@
 <script setup>
 
 import Modal from "../components/Modal.vue";
+import axios from "axios";
+import useMovieStore from "../store/index.js"
+
+// const getMovies = async () => {
+//     await (axios.get (`https://api.themoviedb.org/3/trending/movie/day?language=en-US`))
+// }
 </script>
 
 <template>
